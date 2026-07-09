31a473a9d2f392870859123e25f9b976aeeb95b0
Ranjeet Shinde <ranjeetshinde2212@gmail.com>
2026-07-09 20:54:06 +0530

    Time: 25 ms (49.04%), Space: 228.9 MB (47.4%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    int find(vector<int> &parent, int node){
-        if(parent[node] != node){
+    int find(vector<int>& parent, int node) {
+        if (parent[node] != node) {
             parent[node] = find(parent, parent[node]);
         }
         return parent[node];
     }
-    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
+                                      vector<vector<int>>& queries) {
         int m = nums.size();
         vector<int> parent(m);
-        for(int i = 0; i < m; i++) parent[i] = i;
+        for (int i = 0; i < m; i++)
+            parent[i] = i;
 
-        for(int i = 0, j = 1; j < m;){
-            if(abs(nums[i] - nums[j]) <= maxDiff){
+        for (int i = 0, j = 1; j < m;) {
+            if (abs(nums[i] - nums[j]) <= maxDiff) {
                 parent[j] = i;
                 j++;
             } else {
                 i++;
-                if(i == j) j++;
+                if (i == j)
+                    j++;
             }
         }
 
         vector<bool> r;
-        for(const auto &q : queries){
-            if(find(parent, q[0]) == find(parent, q[1])) r.push_back(true);
-            else r.push_back(false);
+        for (const auto& q : queries) {
+            r.push_back(find(parent, q[0]) == find(parent, q[1]));
         }
         return r;
     }
