bd54e972f4d3f6444956fd96817202837b245189
Ranjeet Shinde <ranjeetshinde2212@gmail.com>
2026-07-21 18:39:39 +0530

    Time: 3 ms (17.54%), Space: 14.4 MB (51.81%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        int n = tokens.size();
+
+        sort(tokens.begin(), tokens.end());
+        if (n == 0 || tokens[0] > power)
+            return 0;
+
+        int score = 0;
+        int i = 0, e = n - 1;
+
+        for (; i < e;) {
+            if (tokens[i] > power && score) {
+                score--;
+                power += tokens[e--];
+            } else {
+                power -= tokens[i++];
+                score++;
+            }
+        }
+
+        if (i == e && tokens[i] <= power)
+            return ++score;
+            
+        return score;
+    }
+};
