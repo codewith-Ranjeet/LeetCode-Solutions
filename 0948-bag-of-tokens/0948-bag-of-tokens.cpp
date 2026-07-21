72eeb4acad69962393cd3d3cb55a347ba9428a37
Ranjeet Shinde <ranjeetshinde2212@gmail.com>
2026-07-21 18:46:41 +0530

    Time: 4 ms (7.8%), Space: 14.4 MB (51.81%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -7,10 +7,10 @@ public:
         if (n == 0 || tokens[0] > power)
             return 0;
 
-        int score = 0;
+        int score = 0, max_score = 0;
         int i = 0, e = n - 1;
 
-        for (; i < e;) {
+        for (; i <= e;) {
             if (tokens[i] > power) {
                 if(!score) break;
                 score--;
@@ -18,12 +18,10 @@ public:
             } else {
                 power -= tokens[i++];
                 score++;
+                max_score = max(max_score, score);
             }
         }
-
-        if (i == e && tokens[i] <= power)
-            return ++score;
             
-        return score;
+        return max_score;
     }
 };
